ca9018908425e5e5c9cb18cfa719905fa0ec533b
Ujjwal <aroraujjwal7@gmail.com>
2016-10-11 15:42:11 +0530

    Module2 Edited

--- js/app.js
+++ js/app.js
@@ -7,22 +7,33 @@
     ToBuyController.$inject = ['ShoppingListService'];
     function ToBuyController(ShoppingListService){
         var buy = this;
-        buy.listOfItems = ShoppingListService.getListOfItemsToBuy();
-        buy.message = false;
-        buy.removeFromList = function(index){
-            try{
-                ShoppingListService.removeItemFromList(index);      
-            } catch(error) {
-                buy.message = true;
-            }   
+        
+        buy.listOfItems = function(){
+          return  ShoppingListService.getListOfItemsToBuy();
         };
+        
+        buy.removeFromList = function(index){
+            ShoppingListService.removeItemFromList(index);      
+        };
+        
+        buy.message = function(){
+         return (buy.listOfItems().length === 0);   
+        };
+        
     };
     
     AlreadyBoughtController.$inject = ['ShoppingListService'];
     function AlreadyBoughtController(ShoppingListService){
         var bought = this;
-        bought.listOfItems = ShoppingListService.getListOfItemsBought();
-        bought.message = false;
+        
+        bought.listOfItems = function() {
+         
+            return ShoppingListService.getListOfItemsBought();
+        };
+        
+        bought.message = function(){
+            return (bought.listOfItems().length === 0);
+        };
     };
     
     function ShoppingListService(){
@@ -31,14 +42,9 @@
         var itemsToBuy = [{name: "Cookies", quantity: 12}, {name: "Cakes", quantity: 5}, {name: "Soda", quantity: 15}, {name: "Pepto Bismol", quantity: 3}, {name: "Candies", quantity: 20}];
         var itemsBought = [];
         
-        service.boughtMessage = function(){
-            console.log("Function Called");
-            return (itemsBought.length === 0);
-        };
-        
         service.getListOfItemsToBuy = function(){
             return itemsToBuy;
-        }; 
+        };
         
         service.getListOfItemsBought = function(){
             return itemsBought;
@@ -48,12 +54,6 @@
             var item = itemsToBuy[index];
             itemsToBuy.splice(index,1);
             itemsBought.push(item);
-            if(itemsToBuy.length === 0){
-                throw new Error();
-            }
-            if(itemsBought.length !== 0){
-                service.boughtMessage = false;
-            }
         };
         
     };
